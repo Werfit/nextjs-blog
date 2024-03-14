08b00046480963a503da34da00e0d8369608a57e
Werfit <iskenderov.vsl@gmail.com>
2024-03-14 23:34:46 +0200

    fix: auth options export moved to a separate file

--- blog/src/app/api/auth/[...nextauth]/route.ts
+++ blog/src/app/api/auth/[...nextauth]/route.ts
@@ -1,56 +1,6 @@
-import NextAuth, { AuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { User } from "@/types/user.type";
-import { loginUser } from "./auth";
+import NextAuth from "next-auth";
 
-export const authOptions: AuthOptions = {
-  callbacks: {
-    jwt({ token, user }) {
-      // user that comes from the authorization is of custom type User rather User | AdapterUser from the next-auth
-      const authorizedUser: User = user as User;
-
-      if (user) {
-        return {
-          ...token,
-          user: { id: authorizedUser.id, username: authorizedUser.username },
-        };
-      }
-
-      return token;
-    },
-    session({ session, token }) {
-      if (token.user) {
-        session.user = token.user as User;
-      }
-
-      return session;
-    },
-  },
-  providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        username: {
-          type: "text",
-        },
-        password: {
-          type: "password",
-        },
-      },
-      async authorize(credentials) {
-        if (!credentials) {
-          return null;
-        }
-
-        const user = await loginUser(
-          credentials.username,
-          credentials.password
-        );
-        return user;
-      },
-    }),
-  ],
-};
+import { authOptions } from "./options";
 
 const handlers = NextAuth(authOptions);
 
